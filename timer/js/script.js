e49d8fae298cf25ecc64526c6d2e766c6f329df8
Yevgen <notky@ukr.net>
2019-09-26 09:56:04 +0300

    add class Timer

--- timer/js/script.js
+++ timer/js/script.js
@@ -3,53 +3,68 @@ window.addEventListener('DOMContentLoaded', function(){
 
     // Timer
 
-    let deadline = "2019-09-20 16:31:00",
-        id = 'timer';
-
-    function setClock(id, endTime){
-        let timer = document.getElementById(id),
-            hours = timer.querySelector('.hours'),
-            minutes = timer.querySelector('.minutes'),
-            seconds = timer.querySelector('.seconds'),
-            timeInterval = setInterval(updateClock, 1000);
-
-            function getTimeRemaining(endTime){
-                let t = Date.parse(endTime) - Date.parse(new Date()),
-                seconds = Math.floor(t/1000 % 60),
-                minutes = Math.floor(t/(1000*60) % 60),
-                hours = Math.floor(t/(1000*60*60));
+    class Timer{
+        constructor(id = 'timer', deadline = new Date()){
+            this._fieldTimer = document.getElementById(id);
+            this._fieldHours = this._fieldTimer.querySelector('.hours');
+            this._fieldMinutes = this._fieldTimer.querySelector('.minutes');
+            this._fieldSeconds = this._fieldTimer.querySelector('.seconds');
+            this._deadline = deadline;
+            this._updateClock = this._updateClock.bind(this);
+        }
     
-                return {
-                    'total' : t,
-                    'hours' : hours,
-                    'minutes' : minutes,
-                    'seconds' : seconds
-                };
+        _setTimeRemaining(){
+            this._total = Date.parse(this._deadline) - Date.parse(new Date());
+            this._seconds = Math.floor(this._total/1000 % 60);
+            this._minutes = Math.floor(this._total/(1000*60) % 60);
+            this._hours = Math.floor(this._total/(1000*60*60));
+        }
+    
+        _editNumber(){
+            this._hours = this._addZero(this._hours);
+            this._minutes = this._addZero(this._minutes);
+            this._seconds = this._addZero(this._seconds);        
+        }
+    
+        _addZero(num){
+            if(num < 10){
+                return '0' + num;
+            } else {       
+                return num;
             }
-
-            function addZero(num){
-                if(num < 10){
-                    return '0' + num;
-                } else {         
-                    return num;
-                }
+        }
+    
+        _validateEndTime(){
+            if(this._total <= 0){
+                this.stop();
+                this._hours = '00';
+                this._minutes = '00';
+                this._seconds = '00';
             }
+        }
+    
+        _updateClock(){
+            this._setTimeRemaining();
+            this._editNumber();
+            this._validateEndTime();
+            this._showData();
+        }
 
-            function updateClock(){
-                let t = getTimeRemaining(endTime);
-
-                hours.textContent = addZero(t.hours);
-                minutes.textContent = addZero(t.minutes);
-                seconds.textContent = addZero(t.seconds);
-
-                if(t.total <= 0){
-                    clearInterval(timeInterval);
-                    hours.textContent = '00';
-                    minutes.textContent = '00';
-                    seconds.textContent = '00';
-                }
-            }
+        _showData(){
+            this._fieldHours.textContent = this._hours;
+            this._fieldMinutes.textContent = this._minutes;
+            this._fieldSeconds.textContent = this._seconds;
+        }
+    
+        start(){
+            this.interval = setInterval(this._updateClock, 1000);
+        }
+    
+        stop(){
+            clearInterval(this.interval);
+        }
     }
-
-    setClock(id, deadline);
+    
+    new Timer('timer', '2020-09-24 12:55:00').start();
+    //new Timer().start();
 });
