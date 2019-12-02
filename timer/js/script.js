81224bd4f80a2b56f5fffda70d64f1691bbcfa29
Zarechniy Yevgen <36895300+Notkyn@users.noreply.github.com>
2019-12-02 10:45:33 +0200

    Update script.js

--- timer/js/script.js
+++ timer/js/script.js
@@ -1,70 +1,61 @@
-window.addEventListener('DOMContentLoaded', function(){
-    'use strict';
+export class Timer{
+    constructor(id = 'timer', deadline = new Date()){
+        this._fieldTimer = document.getElementById(id);
+        this._fieldHours = this._fieldTimer.querySelector('.hours');
+        this._fieldMinutes = this._fieldTimer.querySelector('.minutes');
+        this._fieldSeconds = this._fieldTimer.querySelector('.seconds');
+        this._deadline = deadline;
+        this._updateClock = this._updateClock.bind(this);
+    }
 
-    // Timer
+    _setTimeRemaining(){
+        this._total = Date.parse(this._deadline) - Date.parse(new Date());
+        this._seconds = Math.floor(this._total/1000 % 60);
+        this._minutes = Math.floor(this._total/(1000*60) % 60);
+        this._hours = Math.floor(this._total/(1000*60*60));
+    }
 
-    class Timer{
-        constructor(id = 'timer', deadline = new Date()){
-            this._fieldTimer = document.getElementById(id);
-            this._fieldHours = this._fieldTimer.querySelector('.hours');
-            this._fieldMinutes = this._fieldTimer.querySelector('.minutes');
-            this._fieldSeconds = this._fieldTimer.querySelector('.seconds');
-            this._deadline = deadline;
-            this._updateClock = this._updateClock.bind(this);
-        }
-    
-        _setTimeRemaining(){
-            this._total = Date.parse(this._deadline) - Date.parse(new Date());
-            this._seconds = Math.floor(this._total/1000 % 60);
-            this._minutes = Math.floor(this._total/(1000*60) % 60);
-            this._hours = Math.floor(this._total/(1000*60*60));
-        }
-    
-        _editNumber(){
-            this._hours = this._addZero(this._hours);
-            this._minutes = this._addZero(this._minutes);
-            this._seconds = this._addZero(this._seconds);        
-        }
-    
-        _addZero(num){
-            if(num < 10){
-                return '0' + num;
-            } else {       
-                return num;
-            }
-        }
-    
-        _validateEndTime(){
-            if(this._total <= 0){
-                this.stop();
-                this._hours = '00';
-                this._minutes = '00';
-                this._seconds = '00';
-            }
-        }
-    
-        _updateClock(){
-            this._setTimeRemaining();
-            this._editNumber();
-            this._validateEndTime();
-            this._showData();
-        }
+    _editNumber(){
+        this._hours = this._addZero(this._hours);
+        this._minutes = this._addZero(this._minutes);
+        this._seconds = this._addZero(this._seconds);
+    }
 
-        _showData(){
-            this._fieldHours.textContent = this._hours;
-            this._fieldMinutes.textContent = this._minutes;
-            this._fieldSeconds.textContent = this._seconds;
-        }
-    
-        start(){
-            this.interval = setInterval(this._updateClock, 1000);
-        }
-    
-        stop(){
-            clearInterval(this.interval);
+    _addZero(num){
+        if(num < 10){
+            return '0' + num;
+        } else {
+            return num;
         }
     }
-    
-    new Timer('timer', '2020-09-24 12:55:00').start();
-    //new Timer().start();
-});
+
+    _validateEndTime(){
+        if(this._total <= 0){
+            this.stop();
+            this._hours = '00';
+            this._minutes = '00';
+            this._seconds = '00';
+        }
+    }
+
+    _updateClock(){
+        this._setTimeRemaining();
+        this._editNumber();
+        this._validateEndTime();
+        this._showData();
+    }
+
+    _showData(){
+        this._fieldHours.textContent = this._hours;
+        this._fieldMinutes.textContent = this._minutes;
+        this._fieldSeconds.textContent = this._seconds;
+    }
+
+    start(){
+        this.interval = setInterval(this._updateClock, 1000);
+    }
+
+    stop(){
+        clearInterval(this.interval);
+    }
+}
